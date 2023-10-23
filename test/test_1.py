479996870c80449af38e6266bd17d4322546a417
javierac11 <javieracedocastrillon@gmail.com>
2023-10-23 18:19:30 +0200

    Ejercicio 2 + test

--- test/test_1.py
+++ test/test_1.py
@@ -2,11 +2,9 @@ from src.ejercicio1 import leeEdad
 import pytest
 
 
-def test_leeEdad_valida(monkeypatch):
+def test_1(monkeypatch):
     monkeypatch.setattr('builtins.input', lambda _: '25')
     assert leeEdad() == 25
-
-def test_leeEdad_invalida_texto(monkeypatch, capsys):
     monkeypatch.setattr('builtins.input', lambda _: 'abc')
     with pytest.raises(ValueError):
         leeEdad()
